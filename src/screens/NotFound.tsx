e77b7cefeb3afcd412538341d33a883040de7a2b
Jo Harijaonina <jo.harijaonina@gmail.com>
2023-08-10 10:47:16 +0300

    fixed NotFound screen

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
-    <div className="grid place-items-center h-[calc(100vh-70px)] text-white text-xl font-bold">
-      404 - Page Not Found
+    <div className="min-h-screen">
+      <div className="grid place-items-center h-[calc(100vh-70px)] text-white text-xl font-bold">
+        404 - Page Not Found
+      </div>
     </div>
   );
 };
